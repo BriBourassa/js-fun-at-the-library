99dadbe028515e414f87c487162db213dd0f3533
Brianne Bourassa <shinybri1011@gmail.com>
2022-12-03 21:56:16 -0700

    Complete iteration 3 and clean up notes

--- src/Library.js
+++ src/Library.js
@@ -14,15 +14,6 @@ function addBook(library, book){
     library.shelves[book.genre].push(book)
 }
 
-
-
-// function checkoutBook(library, book, shelfGenre){
-//   for(var i = 0; i < library.length; i++)
-//   if(library.shelves[i].title === book.title){
-//     library.shelves.splice(i,1)
-//   }
-// }
-
 function checkoutBook(library, book, shelfGenre){
   for(var i = 0; i < library['shelves'][shelfGenre].length; i++) {
     if(library['shelves'][shelfGenre][i].title === book){
@@ -34,13 +25,6 @@ function checkoutBook(library, book, shelfGenre){
 }
 
 
-// function unshelfBook(bookToRemove, shelf){
-//   for(var i = 0; i < shelf.length; i++) {
-//     if([shelf][i].title === bookToRemove){
-//       [shelf].splice(i,1)
-//     }
-//   }  
-// }
 
 
 module.exports = {
