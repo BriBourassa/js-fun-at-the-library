3d1b94f4cc85cd868575b0ec1e31a66fd57c4308
Brianne Bourassa <shinybri1011@gmail.com>
2022-12-03 21:54:43 -0700

    Complete Library-test and iteration 3

--- src/Library.js
+++ src/Library.js
@@ -11,19 +11,40 @@ function createLibrary(name){
 }
 
 function addBook(library, book){
-
-  for(var i = 0; i < library.shelves.length; i++)
-
-  if(book.genre === library[i].genre){
-    library.shelves[i].push(book)
-  }
+    library.shelves[book.genre].push(book)
 }
-// if genre matches shelf genre, .push
 
+
+
+// function checkoutBook(library, book, shelfGenre){
+//   for(var i = 0; i < library.length; i++)
+//   if(library.shelves[i].title === book.title){
+//     library.shelves.splice(i,1)
+//   }
+// }
+
+function checkoutBook(library, book, shelfGenre){
+  for(var i = 0; i < library['shelves'][shelfGenre].length; i++) {
+    if(library['shelves'][shelfGenre][i].title === book){
+      library['shelves'][shelfGenre].splice(i,1)
+      return `You have now checked out ${book} from the ${library.name}`
+    } 
+  }  
+  return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
+}
+
+
+// function unshelfBook(bookToRemove, shelf){
+//   for(var i = 0; i < shelf.length; i++) {
+//     if([shelf][i].title === bookToRemove){
+//       [shelf].splice(i,1)
+//     }
+//   }  
+// }
 
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
