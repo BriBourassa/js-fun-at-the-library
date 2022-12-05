96bf872fe7575f2cd62c0c8390ad3bff0547fc31
Brianne Bourassa <shinybri1011@gmail.com>
2022-12-04 17:03:33 -0700

    Complete first seven test in librarian-test

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,24 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name,
+    this.library = library
   }
+
+  greetPatron(name, dayTime){
+    if(dayTime === true){
+      return `Good morning, ${name}!`
+    }
+    return `Hello, ${name}!`
+  }
+
+  findbook(librarian, book){
+    for(var i = 0; i < library['shelves'][shelfGenre].length; i++)
+    if(librarian.library['shelves'][shelfgenre][i].title === book.title){
+      console.log
+      return `Yes, we have ${book.title}`
+    }
+    return `Sorry, we do not have ${book.title}`
 }
+} 
 
 module.exports = Librarian;
